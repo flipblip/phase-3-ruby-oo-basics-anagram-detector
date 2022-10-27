a6dc0b7ef07ce404d289c1d3ab2408378d93142e
fliblip <wickynjy@gmail.com>
2022-10-27 10:41:26 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @split_word = word.chars.sort
+    end
+
+    def match array
+        anagrams = array.filter do |word|
+            @split_word == word.chars.sort
+        end
+    end
+end
